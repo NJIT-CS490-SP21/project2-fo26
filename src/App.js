d8ba7b2f53c842ca1bfc2fdd89e12bb3cf8d1a38
Fernando Olivera <foliverjr@gmail.com>
2021-02-19 05:04:43 +0000

    Add clickable tic-tac-toe board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Board, Square } from './Board.js';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io();
 
 function App() {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="board">
+            <Board />
+        </div>
     </div>
   );
 }
---
+++ src/Board.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import io from 'socket.io-client';
+
+const socket = io(); // Connects to socket connection
+
+export const Square = (props) => {
+    const onClickBox = () => {
+        let newBoard = [...props.Board]
+        
+        if (newBoard[props.index] == 'X')
+            newBoard[props.index] = 'O'
+        else
+            newBoard[props.index] = 'X'
+            
+        props.setBoard(newBoard)
+        
+        socket.emit('move', {
+            index: props.index,
+            player: newBoard[props.index]
+        });
+    }
+    
+    return (
+        <div className="box" onClick={onClickBox}>
+            {props.value}
+        </div>
+    )
+}
+
+
+export const Board = () => {
+    const [Board, setBoard] = useState(['','','','','','','','','']);
+    useEffect(() => {
+        socket.on('move', (data) => {
+          console.log(data);
+          setBoard((prevBoard) => {
+            let newBoard = [...prevBoard]
+            newBoard[data.index] = data.player
+            return newBoard
+          });
+        })
+    }, ['','','','','','','','','']);
+    
+    return (
+        <div className="board">
+            {Board.map((squareVal, index) => 
+                <Square 
+                    Board={Board} setBoard={setBoard} 
+                    value={squareVal} index={index} 
+                />
+            )}
+        </div>
+    )
+}
